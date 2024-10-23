3300a3e7ef3e62bc89a09b385933fa33b144a15b
EmanuelVMec <joshua.vinces5503@utc.edu.ec>
2024-10-23 13:03:34 -0500

    Añadido el cálculo del perímetro al área del cuadrado en la versión 1.2

--- area.py
+++ area.py
@@ -3,3 +3,6 @@ l1 = int(input("Ingrese el lado 1: "));
 l2 = int(input("Ingrese el lado 2: "));
 area = l1 * l2;
 print(f"El area del cuadrado es: {area}");
+
+perimetro = 2 * (l1 + l2)
+print(f"El perímetro del cuadrado es: {perimetro}")
