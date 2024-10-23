dfa61cdff5378778d1b508e084449ff79fd96922
EmanuelVMec <joshua.vinces5503@utc.edu.ec>
2024-10-23 12:37:35 -0500

    Create area.py

---
+++ area.py
@@ -0,0 +1,5 @@
+print("CALCULAR EL AREA DEL CUADRADO");
+l1 = int(input("Ingrese el lado 1: "));
+l2 = int(input("Ingrese el lado 2: "));
+area = l1 * l2;
+print(f"El area del cuadrado es: {area}");
